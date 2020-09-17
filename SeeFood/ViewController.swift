1e189a838f1a5208cb4e125bda3c219a6412fb61
Yesbolat Syilybay <beskaska@gmail.com>
2020-09-17 12:35:48 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Yesbolat Syilybay on 17.09.2020.
+//  Copyright © 2020 conizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
